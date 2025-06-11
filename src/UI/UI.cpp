ff5cfa8a668a494133f56500e110df4e06efbbe9
astrogames06 <jebba272727@gmail.com>
2025-06-12 08:12:24 +1000

    changed the play & stop button

--- src/UI/UI.cpp
+++ src/UI/UI.cpp
@@ -20,12 +20,6 @@ void DrawUI()
 {
     if (game.scene == SCENE::EDITOR)
     {
-        if (GuiButtonRounded({(float)game.WIDTH-110, 10, 100, 45}, "PLAY!", 5.f, 20.f, game.UI_recs))
-        {
-            game.scene = SCENE::GAME;
-            return;
-        }
-
         if (GuiButtonRounded({10, 10, 45, 45}, "#23#", 5.f, 5.f, game.UI_recs))
         {
             game.mode = BLOCK;
@@ -61,6 +55,13 @@ void DrawUI()
             game.mode = SPIKES;
             mode_icon = 221;
         }
+
+        if (GuiButtonRounded({(float)game.WIDTH-55, 10, 45, 45}, "#131#", 5.f, 20.f, game.UI_recs))
+        {
+            game.scene = SCENE::GAME;
+            return;
+        }
+
         // DrawText(std::to_string(game.entities.size()).c_str(), 20, 20, 20, BLACK);
         if (GuiButtonRounded({(float)10, (float)game.HEIGHT-55, 100, 45}, "LOAD GAME!", 5.f, 20.f, game.UI_recs))
         {
@@ -77,7 +78,7 @@ void DrawUI()
 
     else if (game.scene == SCENE::GAME)
     {
-        if (GuiButtonRounded({(float)game.WIDTH-110, 10, 100, 45}, "STOP!", 5.f, 20.f, game.UI_recs))
+        if (GuiButtonRounded({(float)game.WIDTH-55, 10, 45, 45}, "#132#", 5.f, 20.f, game.UI_recs))
         {
             game.Reset();
             return;
