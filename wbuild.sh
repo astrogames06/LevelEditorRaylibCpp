ef4dd2a9b57090a2ba532604d7da4c7cd07f971c
Jesse Sher <jebba272727@gmail.com>
2025-07-12 15:24:20 +1000

    e

--- wbuild.sh
+++ wbuild.sh
@@ -1,3 +1,3 @@
 FILES=$(find src/ -name "*.cpp")
 
-em++ -o index.html $FILES -std=c++17 -Os -Wall ./lib/libraylib.web.a -I. -I include/ -L. -L lib/ -s USE_GLFW=3 --shell-file shell.html -DPLATFORM_WEB
+em++ -o index.html $CODE_FILES -std=c++17 -Os -Wall ./lib/libraylib.web.a -I. -Iinclude/ -L. -Llib/ -s USE_GLFW=3 --preload-file assets --shell-file shell.html -DPLATFORM_WEB -sINITIAL_MEMORY=33554432 -sALLOW_MEMORY_GROWTH=1 -sSTACK_SIZE=10485760 -sEXPORTED_FUNCTIONS=["_main","_SaveDataJSON","_LoadDataJSON"] -sEXPORTED_RUNTIME_METHODS=["ccall"] -sASSERTIONS=2
